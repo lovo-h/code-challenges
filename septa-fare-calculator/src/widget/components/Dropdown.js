9bb70b82efe4d5299eb094bed2725653d0315323
Hector L <lovohh@gmail.com>
2025-09-07 23:50:00 -0400

    DropDown: Resize Width Based On Content

--- septa-fare-calculator/src/widget/components/Dropdown.js
+++ septa-fare-calculator/src/widget/components/Dropdown.js
@@ -1,19 +1,45 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
+import { dayTypeLabelLookup } from '../utils/common';
 
 function DropDown( { options, value, onChange } ) {
+  const spanRef = useRef();
+  const [ width, setWidth ] = React.useState( 'auto' );
+  useEffect( () => {
+    if ( ! spanRef.current ) {
+      return;
+    }
+
+    setWidth( spanRef.current.offsetWidth + 100 );
+  }, [ value ] );
+
   // Return early if no options.
   if ( ! options || options.length === 0 ) {
     return null;
   }
 
+
+  const spanStyle = {
+    // TODO: fontSize should not be hardcoded but derived from CSS.
+    fontSize: '1.6rem',
+    position: 'absolute',
+    visibility: 'hidden',
+    whiteSpace: 'nowrap',
+    font: 'inherit'
+  };
+
   return (
-    <select className="dropdown" value={ value } onChange={ onChange }>
-      { options.map( ( option ) => (
-        <option key={ option.value } value={ option.value }>
-          { option.label }
-        </option>
-      ) ) }
-    </select>
+    <>
+      <select className="dropdown" style={ { width } } value={ value } onChange={ onChange }>
+        { options.map( ( option ) => (
+          <option key={ option.value } value={ option.value }>
+            { option.label }
+          </option>
+        ) ) }
+      </select>
+      <span ref={ spanRef } style={ spanStyle }>
+        { dayTypeLabelLookup[ value ] || value }
+      </span>
+    </>
   );
 }
 
